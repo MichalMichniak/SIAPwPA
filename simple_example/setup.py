8f95531bdf6adf494bca19668d06f1f8121bfea8
zojapila@gmail.com <zojapila@gmail.com>
2024-11-27 13:45:13 +0000

    dzialajace strzałki i teleop

--- simple_example/setup.py
+++ simple_example/setup.py
@@ -21,7 +21,7 @@ setup(
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
         (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*')))] 
-                + generate_data_files('share/'+package_name+'/', 'models'),
+                # + generate_data_files('share/'+package_name+'/', 'models'),
     # data_files=[
         # ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         # ('share/' + package_name, ['package.xml']),
