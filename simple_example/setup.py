8e4e639dedd1ee0d9d8889e022e8cec3bcb965c6
MichalMichniak <michalmichniak2001@gmail.com>
2024-12-01 18:25:04 +0100

    commit

--- simple_example/setup.py
+++ simple_example/setup.py
@@ -20,18 +20,8 @@ setup(
     data_files=[('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*')))] 
-                # + generate_data_files('share/'+package_name+'/', 'models'),
-    # data_files=[
-        # ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
-        # ('share/' + package_name, ['package.xml']),
-        # (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-    #     # (os.path.join('share', package_name, 'models/vehicle'), glob(os.path.join('models/vehicle', '*'))),
-    #     # (os.path.join('share', package_name, 'models/vehicle/prius'), glob(os.path.join('models/vehicle/prius', '*'))),
-    #     (os.path.join('share', package_name, 'models/vehicle/prius'), glob(os.path.join('models/vehicle/prius', '*'))),
-    #     (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*'))),
-
-    # ],
+        (os.path.join('share', package_name, 'worlds'), glob(os.path.join('worlds', '*')))
+        ], 
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='developer',
